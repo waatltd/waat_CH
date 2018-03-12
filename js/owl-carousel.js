ee8e8341b0fedcafc77387fcb730d6401e30f648
Tomek Pazio <tomasz1pazio@gmail.com>
2018-03-12 16:39:56 +0100

    Updates

---
+++ js/owl-carousel.js
@@ -0,0 +1,2 @@
+
+$(document).ready(function(){$('.owl-carousel').owlCarousel({autoplay:true,items:1,smartSpeed:1000,loop:true,margin:50,nav:true,navText:["<i class='icon icon--arrow-left'>","<i class='icon icon--arrow-right'>"]});});
